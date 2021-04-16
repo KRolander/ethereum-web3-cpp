af40b86019085b3de72d7f3165ebb81da1f63d38
Roland <roland.kromes@etu.univ-cotedazur.fr>
2021-04-16 08:32:33 +0200

    Web3 tools for sending transaction with ethereum methods

---
+++ src/Web3.cpp
@@ -0,0 +1,294 @@
+//
+//   Author: Roland Kromes, @ roland.kromes@univ-cotedazur.fr
+//   Address: LEAT Laboratory, Université Côte d'Azur
+//
+//   The Web3 class is used to RPC format, last step before sending 
+//   the transaction to the Ethereum RPC interface
+//
+
+#include "Web3.h"
+#include "Util.h"
+#include "cJSON.h"
+#include <iostream>
+#include <sstream>
+
+
+
+Web3::Web3(const string* _host, const string* _path) {
+    // client.setCACert(infura_ca_cert);
+    host = _host;
+    path = _path;
+}
+
+string Web3::Web3ClientVersion() {
+    string m = "web3_clientVersion";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getString(&output);
+}
+
+string Web3::Web3Sha3(const string* data) {
+    string m = "web3_sha3";
+    string p = "[\"" + *data + "\"]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getString(&output);
+}
+
+int Web3::NetVersion() {
+    string m = "net_version";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getInt(&output);
+}
+
+bool Web3::NetListening() {
+    string m = "net_listening";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getBool(&output);
+}
+
+int Web3::NetPeerCount() {
+    string m = "net_peerCount";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getInt(&output);
+}
+
+double Web3::EthProtocolVersion() {
+    string m = "eth_protocolVersion";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getDouble(&output);
+}
+
+bool Web3::EthSyncing() {
+    string m = "eth_syncing";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string result = exec(&input);
+
+    cJSON *root, *value;
+    root = cJSON_Parse(result.c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    bool ret;
+    if (cJSON_IsBool(value)) {
+        ret = false;
+    } else{
+        ret = true;
+    }
+    cJSON_free(root);
+    return ret;
+}
+
+bool Web3::EthMining() {
+    string m = "eth_mining";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getBool(&output);
+}
+
+double Web3::EthHashrate() {
+    string m = "eth_hashrate";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getDouble(&output);
+}
+
+long long int Web3::EthGasPrice() {
+    string m = "eth_gasPrice";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getLongLong(&output);
+}
+
+void Web3::EthAccounts(char** array, int size) {
+     // TODO
+}
+
+int Web3::EthBlockNumber() {
+    string m = "eth_blockNumber";
+    string p = "[]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getInt(&output);
+}
+
+long long int Web3::EthGetBalance(const string* address) {
+    string m = "eth_getBalance";
+    string p = "[\"" + *address + "\",\"latest\"]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getLongLong(&output);
+}
+
+int Web3::EthGetTransactionCount(const string* address) {
+    string m = "eth_getTransactionCount";
+    string p = "[\"" + *address + "\",\"latest\"]";
+    string input = generateJson(&m, &p);
+    string output = exec(&input);
+    return getInt(&output);
+}
+
+string Web3::EthCall(const string* from, const string* to, long gas, long gasPrice,
+                     const string* value, const string* data) {
+    // TODO use gas, gasprice and value
+    string m = "eth_call";
+    string p = "[{\"from\":\"" + *from + "\",\"to\":\""
+               + *to + "\",\"data\":\"" + *data + "\"}, \"latest\"]";
+    string input = generateJson(&m, &p);
+    return exec(&input);
+}
+
+string Web3::EthSendSignedTransaction(const string* data, const uint32_t dataLen) {
+    string m = "eth_sendRawTransaction";
+    string p = "[\"" + *data + "\"]";
+    string input = generateJson(&m, &p);
+#if 0
+    LOG(input);
+#endif
+    // TODO : 
+    // return exec(&input);
+
+    std::cout << "TODO in EthSendSignedTransaction" << std::endl;
+    return input;
+}
+
+// -------------------------------
+// Private
+
+string Web3::generateJson(const string* method, const string* params) {
+    return "{\"jsonrpc\":\"2.0\",\"method\":\"" + *method + "\",\"params\":" + *params + ",\"id\":0}";
+}
+
+string Web3::exec(const string* data) {
+    string result;
+
+    // start connection
+    // LOG("\nStarting connection to server...");
+    // int connected = client.connect(host->c_str(), 443);
+    // if (!connected) {
+    //     return "";
+    // }
+
+    // LOG("Connected to server!");
+    // Make a HTTP request:
+    int l = data->size();
+    stringstream ss;
+    ss << l;
+    string lstr = ss.str();
+
+    string strPost = "POST " + *path + " HTTP/1.1";
+    string strHost = "Host: " + *host;
+    string strContentLen = "Content-Length: " + lstr;
+
+    // client.println(strPost.c_str());
+    // client.println(strHost.c_str());
+    // client.println("Content-Type: application/json");
+    // client.println(strContentLen.c_str());
+    // client.println("Connection: close");
+    // client.println();
+    // client.println(data->c_str());
+
+    // while (client.connected()) {
+    //     String line = client.readStringUntil('\n');
+    //     // LOG(line.c_str());
+    //     if (line == "\r") {
+    //         break;
+    //     }
+    // }
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    // while (client.available()) {
+    //     char c = client.read();
+    //     result += c;
+    // }
+    // LOG(result.c_str());
+
+    // client.stop();
+
+    // return result;
+    return 0;
+}
+
+int Web3::getInt(const string* json) {
+    int ret = -1;
+    cJSON *root, *value;
+    root = cJSON_Parse(json->c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    if (cJSON_IsString(value)) {
+        ret = strtol(value->valuestring, nullptr, 16);
+    }
+    cJSON_free(root);
+    return ret;
+}
+
+long Web3::getLong(const string* json) {
+    long ret = -1;
+    cJSON *root, *value;
+    root = cJSON_Parse(json->c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    if (cJSON_IsString(value)) {
+        ret = strtol(value->valuestring, nullptr, 16);
+    }
+    cJSON_free(root);
+    return ret;
+}
+
+long long int Web3::getLongLong(const string* json) {
+    long long int ret = -1;
+    cJSON *root, *value;
+    root = cJSON_Parse(json->c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    if (cJSON_IsString(value)) {
+        ret = strtoll(value->valuestring, nullptr, 16);
+    }
+    cJSON_free(root);
+    return ret;
+}
+
+double Web3::getDouble(const string* json) {
+    double ret = -1;
+    cJSON *root, *value;
+    root = cJSON_Parse(json->c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    if (cJSON_IsString(value)) {
+        // LOG(value->valuestring);
+        ret = strtof(value->valuestring, nullptr);
+    }
+    cJSON_free(root);
+    return ret;
+}
+
+bool Web3::getBool(const string* json) {
+    bool ret = false;
+    cJSON *root, *value;
+    root = cJSON_Parse(json->c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    if (cJSON_IsBool(value)) {
+        ret = (bool)value->valueint;
+    }
+    cJSON_free(root);
+    return ret;
+}
+
+string Web3::getString(const string* json) {
+    cJSON *root, *value;
+    root = cJSON_Parse(json->c_str());
+    value = cJSON_GetObjectItem(root, "result");
+    if (cJSON_IsString(value)) {
+        cJSON_free(root);
+        return string(value->valuestring);
+    }
+    cJSON_free(root);
+    return nullptr;
+}
---
+++ src/Web3.h
@@ -0,0 +1,55 @@
+//
+//   Author: Roland Kromes, @ roland.kromes@univ-cotedazur.fr
+//   Address: LEAT Laboratory, Université Côte d'Azur
+//
+//   The Web3 class is used to RPC format, last step before sending 
+//   the transaction to the Ethereum RPC interface
+//
+
+#ifndef ARDUINO_WEB3_WEB3_H
+#define ARDUINO_WEB3_WEB3_H
+
+#include "stdint.h"
+#include <string>
+
+using namespace std;
+
+class Web3 {
+public:
+    Web3(const string* _host, const string* _path);
+    string Web3ClientVersion();
+    string Web3Sha3(const string* data);
+    int NetVersion();
+    bool NetListening();
+    int NetPeerCount();
+    double EthProtocolVersion();
+    bool EthSyncing();
+    bool EthMining();
+    double EthHashrate();
+    long long int EthGasPrice();
+    void EthAccounts(char** array, int size);
+    int EthBlockNumber();
+    long long int EthGetBalance(const string* address);
+    int EthGetTransactionCount(const string* address);
+
+    string EthCall(const string* from, const string* to, long gas, long gasPrice, const string* value, const string* data);
+    string EthSendSignedTransaction(const string* data, const uint32_t dataLen);
+
+private:
+    string exec(const string* data);
+    string generateJson(const string* method, const string* params);
+    int getInt(const string* json);
+    long getLong(const string* json);
+    long long int getLongLong(const string* json);
+    double getDouble(const string* json);
+    bool getBool(const string* json);
+    string getString(const string* json);
+
+private:
+    const string* host;
+    const string* path;
+
+
+};
+
+#endif //ARDUINO_WEB3_WEB3_H
