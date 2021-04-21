0f9392fec6fe5216102667bda7ec3a5bcf0700e1
Roland <roland.kromes@etu.univ-cotedazur.fr>
2021-04-21 22:23:56 +0200

    Fix for double signature

--- src/test.cpp
+++ src/test.cpp
@@ -43,7 +43,7 @@ int main()
     std::cout << std::hex << hexStr << std::endl;
 
     // uint8_t toStr[] = CONTRACT_ADDRESS;
-    std::string from = "4bf790d8eb389b31dbe9fe8d357792e61577ed34810b5481c802219afe814e0a"; //"afc46b7fdfc9eb29d67362a6e4df578adeb2eb610af22ae8b3fcb8b3956c3bf3";
+    std::string from = "b37bedd521a67af5cdce434ec9177e2931d82ff92427386d8e8fc8e363d780a4"; //"afc46b7fdfc9eb29d67362a6e4df578adeb2eb610af22ae8b3fcb8b3956c3bf3";
     // Set PrivateKey
     // std::vector<uint8_t> privetKeyVector = Util::ConvertStringToVector(&from);
     // uint8_t * privetKeyArray = privetKeyVector.data();
@@ -60,29 +60,25 @@ int main()
         std::cout << (uint32_t)privateKeyBytes[i] << ' ';
     }
     std::cout << std::endl;
-
     // std::string uintToStr = (char *) toStr;
-    uint32_t nonceVal = 5;
+    uint32_t nonceVal = 2;
     uint64_t gasPriceVal = 0x4A817C800; //46038239233;
     uint32_t gasLimitVal = 0x6691B7;
-    std::string toStr = "0x0154B359b49fE8790A5397A88213d4C99B9aA15C"; //CONTRACT_ADDRESS;
+    std::string toStr = "0x9d98513642716eA671f2b66cA4b5ad0625308eeE"; //CONTRACT_ADDRESS;
     std::string valueStr = "0x00";                      //"0x00";
     uint8_t dataStr[100];
-    std::string func = "my_set(uint256,uint)"; // "my_set(uint8[32])"; //"get(uint256)";
+    std::string func = "setMyData(uint256,uint8,uint256,uint256)"; // "my_set(uint8[32])"; //"get(uint256)";
 
-    Payload transactionPayload; 
-    transactionPayload.signedPayload("db8a042224c44b05a97e5f2a410ea604d818bbe9e6a5d2beed5778e79efd4acf", 0x22, "4bf790d8eb389b31dbe9fe8d357792e61577ed34810b5481c802219afe814e0a");
+    // Payload transactionPayload; 
+    // transactionPayload.signedPayload("db8a042224c44b05a97e5f2a410ea604d818bbe9e6a5d2beed5778e79efd4acf", 0x77, "2a53a7438211849cb95cd4ffe182e01711c34dd525707ab1a0a30fff0b18605b");
     
-    
-   
-    std::cout << "########## Transaction Payload Test" << std::endl;
-    std::cout << "Hash = " << transactionPayload.r << std::endl;
+    // std::cout << "########## Transaction Payload Test" << std::endl;
+    // std::cout << "Hash = " << transactionPayload.r << std::endl;
     uint32_t dataParams = 123;
     char dataString[70]; 
     uint8_t rawdata[32] = {0xdb, 0x8a, 0x04, 0x22, 0x24, 0xc4, 0x4b, 0x05, 0xa9, 0x7e, 0x5f, 0x2a, 0x41, 0x0e, 0xa6, 0x04, 0xd8, 0x18, 0xbb, 0xe9, 0xe6, 0xa5, 0xd2, 0xbe, 0xed, 0x57, 0x78, 0xe7, 0x9e, 0xfd, 0x4a, 0xcf};
     // uint8_t rawdata[32] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-    
-   
+       
     Util::BufToCharStr(dataString, rawdata, 32);
  
     std::cout << "BufToCharStr " << dataString << std::endl;
@@ -90,6 +86,7 @@ int main()
     // char *cstr = new char[dataString.length() + 1];
     // strcpy(cstr, dataString.c_str());
     // uint8_t dest[32]; // = new uint8_t[dataString.length() + 1];
+   
    
    
    
@@ -106,7 +103,11 @@ int main()
     //std::string p2 = transaction.SetupContractData(&func, rawdata);
     int32_t uint8Data = 0x77;
 
-    std::string p2 = transaction.SetupContractData(&func, transactionPayload.r, uint8Data);
+    std::string payloadData = "db8a042224c44b05a97e5f2a410ea604d818bbe9e6a5d2beed5778e79efd4acf";
+    std::string r = "e9014ba4b484ee12141af45cdbd9641e2a053d4f69a103132fb440d27f572695";
+    std::string s = "0dbc83e447221890cbc667ce36f098bfd1bf1df83870236fd35d5317209a83e9";
+
+    std::string p2 = transaction.SetupContractData(&func, payloadData , uint8Data, r , s);
 
     std::cout << "Contract " << p2 << std::endl;
 
